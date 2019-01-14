f0d454beaa45ad8226d9fe5d34a771ef0b9a6f38
Neo <teseus@wemakeprice.com>
2019-01-14 18:30:06 +0900

    add basic files.

---
+++ free-coding/src/main/java/com/teseus/freecoding/FreeCodingApplication.java
@@ -0,0 +1,14 @@
+package com.teseus.freecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCodingApplication.class, args);
+    }
+
+}
+
