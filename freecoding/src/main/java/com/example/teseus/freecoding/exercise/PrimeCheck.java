4b9c3d2893ff8e064dfe1ea381d1537137cd7cc4
Neo <teseus@wemakeprice.com>
2019-07-31 15:35:33 +0900

    add some practices

---
+++ freecoding/src/main/java/com/example/teseus/freecoding/exercise/PrimeCheck.java
@@ -0,0 +1,36 @@
+package com.example.teseus.freecoding.exercise;
+
+public class PrimeCheck {
+
+    private static final int SOURCE = 51;
+
+    public static boolean isPrime(int source) {
+        if (source < 2) {
+            return false;
+        }
+        for (int i = 2; i <= (int) Math.sqrt(source); i++) {
+            if (source % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isOdd(int source) {
+        return (source & 1) == 1;
+    }
+
+    public static boolean isPalindrome(int source) {
+        String original = Integer.toString(source);
+        String reversed = new StringBuilder(Integer.toString(source)).reverse().toString();
+        return original.equals(reversed);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("is the number " + SOURCE + " Prime? : " + isPrime(SOURCE));
+        System.out.println("is the number " + SOURCE + " Odd? : " + isOdd(SOURCE));
+        System.out.println("is the number " + 123455432 + " Palindrome? : " + isPalindrome(123455432));
+        System.out.println("is the number " + 1234554321 + " Palindrome? : " + isPalindrome(1234554321));
+    }
+}
--- freecoding/src/main/java/com/example/teseus/freecoding/freecoding/tdd/spock/after/RangeEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.teseus.freecoding.freecoding.tdd.spock.after;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RangeEntityRepository extends JpaRepository<RangeEntity, Long> {
-}
