ad0274c08e2475debbcc07e60d232b7cff49c9b9
Neo <teseus@wemakeprice.com>
2020-01-17 18:55:30 +0900

    get rid of lombok code when java association.

--- kotlinexercise/src/main/kotlin/com/teseus/grammer/Developer.java
+++ kotlinexercise/src/main/kotlin/com/teseus/grammer/Developer.java
@@ -1,11 +1,19 @@
 package com.teseus.grammer;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Developer {
     private String name;
     private String favorateLanguage;
+
+    public Developer(String name, String favorateLanguage) {
+        this.name = name;
+        this.favorateLanguage = favorateLanguage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavorateLanguage() {
+        return favorateLanguage;
+    }
 }
