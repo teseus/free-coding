8d96302da6238a0bee946c70ef3145ca60026f0d
Neo <teseus@wemakeprice.com>
2020-02-12 17:57:27 +0900

    ValidateBinarySearchTree

--- freecoding/src/main/java/com/teseus/kata/QuickSort.java
+++ freecoding/src/main/java/com/teseus/kata/QuickSort.java
@@ -1,6 +1,45 @@
 package com.teseus.kata;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class QuickSort {
+
+    @Test
+    public void test1(){
+        int[] arr = {9,1,2,7,8,10,11,12};
+        assertArrayEquals(new int[]{1, 2, 7, 8, 9, 10, 11, 12}, QuickSort.sort(arr));
+    }
+
+    @Test
+    public void test2() {
+        assertArrayEquals(QuickSort.sort(
+                new int[]{1, 2, 6, 3, 4, 5}),
+                new int[]{1, 2, 3, 4, 5, 6});
+    }
+
+    @Test
+    public void test3() {
+        assertArrayEquals(QuickSort.sort(
+                new int[]{1,5,9,8,7,4,6,3,2}),
+                new int[]{1,2,3,4,5,6,7,8,9});
+    }
+
+    @Test
+    public void test4() {
+        assertArrayEquals(QuickSort.sort(
+                new int[]{20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0}),
+                new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
+    }
+
+    @Test
+    public void test5() {
+        assertArrayEquals(QuickSort.sort(
+                new int[]{20,14,13,12,11,10,9,19,18,17,16,15,8,7,6,5,4,3,2,1,0}),
+                new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
+    }
+
     public static int[] sort(int[] source) {
         int copied[] = new int[source.length];
         System.arraycopy(source,0,copied,0,source.length);
@@ -22,7 +61,7 @@ public class QuickSort {
     private static int partitioning(int[] arr, int start, int end) {
         int s = start, e = end;
         int medium = arr[(start+end)/2];
-        System.out.println("partitioning start i: " + (start+end)/2 + ", v: " + medium);
+        System.out.println("start medium start i: " + (start+end)/2 + ", v: " + medium);
         printSwapArr("<<", arr, start, end, s, e);
         while(start<=end){
             while(arr[start]<medium)start++;
@@ -32,6 +71,7 @@ public class QuickSort {
                 swap(arr, start, end);
                 printSwapArr(">", arr, start, end, s, e);
                 start++; end--;
+                printSwapArr("=", arr, start, end, s, e);
             }
         }
         printSwapArr(">>", arr, start, end, s, e);
@@ -45,8 +85,8 @@ public class QuickSort {
         }
 
         System.out.println("]"
-                + ", si: " + start + ", sv: " + (start<=(arr.length-1)?arr[start]:"")
-                + ", ei: " + end + ", ev: " + (end>=0?arr[end]:""));
+                + ", index(" + start + ", " + end + ")"
+                + ", value(" + (start<=(arr.length-1)?arr[start]:"") + ", " + (end>=0?arr[end]:"") + ")");
     }
 
     private static void swap(int[] arr, int left, int right) {
---
+++ freecoding/src/main/java/freecoding/exercise/leetcode/ValidateBinarySearchTree.java
@@ -0,0 +1,175 @@
+package freecoding.exercise.leetcode;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ValidateBinarySearchTree {
+    //below successful case after consideration in toilet.
+    public static boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(root.left != null) {
+           if(getMax(root.left) >= root.val){
+               return false;
+           }
+        }
+        if(root.right != null){
+            if(root.val >= getMin(root.right)){
+                return false;
+            }
+        }
+
+        return isValidBST(root.left)
+              && isValidBST(root.right);
+    }
+
+    private static int getMax(TreeNode node) {
+        for(;node.right != null; node = node.right);
+        return node.val;
+    }
+
+    private static int getMin(TreeNode node) {
+        for(;node.left != null; node = node.left);
+        return node.val;
+    }
+
+    //below my fail case and takes too much time but fail.
+    public static boolean isValidBST1(TreeNode root) {
+        return isValidBST(new HashSet<>(),null, root);
+    }
+
+    public static boolean isValidBST(HashSet<Integer> set, TreeNode parent, TreeNode child) {
+        if(child == null){
+            return true;
+        }
+        if (child.left == null && child.right == null) {
+            return !set.contains(child.val);
+        }
+        set.add(child.val);
+        if(valid(parent, child)){
+            if(isValidBST(set, child, child.left) && isValidBST(set, child, child.right))
+                return true;
+            else
+                return false;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean valid(TreeNode parent, TreeNode child){
+        if ((parent != null && child.left != null && child.right != null) && (child.left.val < parent.val && parent.val < child.right.val)) {
+            return false;
+        }
+
+        if(child.left == null){
+            return child.val < child.right.val;
+        }
+        if(null == child.right){
+            return child.left.val < child.val;
+        }
+
+        if(child.left.val < child.val && child.val < child.right.val){
+            return true;
+        }
+        return false;
+    }
+
+    @Test
+    public void test9() {
+        TreeNode node = new TreeNode(3,
+                null,
+                new TreeNode(30,
+                        new TreeNode(10,
+                                null,
+                                new TreeNode(15,
+                                        null,
+                                        new TreeNode(45)))
+                        , null)
+        );
+
+        assertTrue(!ValidateBinarySearchTree.isValidBST(node));
+    }
+
+
+
+    @Test
+    public void test8() {
+        TreeNode node = new TreeNode(3,
+                new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))),
+                new TreeNode(5, new TreeNode(4), new TreeNode(6))
+        );
+
+        assertTrue(!ValidateBinarySearchTree.isValidBST(node));
+    }
+
+    @Test
+    public void test7() {
+        TreeNode node = new TreeNode(10,
+                new TreeNode(5),
+                new TreeNode(15, new TreeNode(6), new TreeNode(20))
+        );
+
+        assertTrue(!ValidateBinarySearchTree.isValidBST(node));
+    }
+
+    @Test
+    public void test4() {
+        TreeNode node = new TreeNode(1,
+                null,
+                new TreeNode(1)
+        );
+
+        assertTrue(!ValidateBinarySearchTree.isValidBST(node));
+    }
+
+    @Test
+    public void test6() {
+        TreeNode node = new TreeNode(5,
+                new TreeNode(1),
+                new TreeNode(4, new TreeNode(3), new TreeNode(6))
+        );
+
+        assertTrue(!ValidateBinarySearchTree.isValidBST(node));
+    }
+
+    @Test
+    public void test1() {
+        TreeNode node = new TreeNode(3,
+                new TreeNode(1),
+                new TreeNode(5, new TreeNode(4), new TreeNode(6))
+        );
+
+        assertTrue(ValidateBinarySearchTree.isValidBST(node));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode node = new TreeNode(2,
+                new TreeNode(1),
+                new TreeNode(3)
+        );
+
+        assertTrue(ValidateBinarySearchTree.isValidBST(node));
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
+        this.left = left;
+        this.right = right;
+    }
+}
