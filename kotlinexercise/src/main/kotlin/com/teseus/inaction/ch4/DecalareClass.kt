38b3da5ca4d7f492918a64a9e23b3616c622f2ea
Neo <teseus@wemakeprice.com>
2020-05-11 12:15:56 +0900

    AbstractProperty

--- kotlinexercise/src/main/kotlin/com/teseus/inaction/ch4/DecalareClass.kt
+++ kotlinexercise/src/main/kotlin/com/teseus/inaction/ch4/DecalareClass.kt
@@ -1,20 +1,24 @@
 package com.teseus.inaction.ch4
 
-class User constructor(val _nickname: String){
+//First way
+class User1 constructor(val _nickname: String){
     val nickname: String
     init {
         nickname = _nickname
     }
 }
 
-//2nd way
-//class User(_nickname: String) {
-//    val nickname = _nickname
-//}
+//Second way
+class User2(_nickname: String) {
+    val nickname = _nickname
+}
 
 //3rd way
-//class User(val nickname:String)
+class User3(val nickname:String)
 
 fun main() {
-
+    //similiar all
+    val user1 = User1("user1")
+    val user2 = User2("user2")
+    val user3 = User3("user3")
 }
