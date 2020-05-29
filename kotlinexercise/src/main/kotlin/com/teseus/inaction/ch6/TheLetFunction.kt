e99ff19ae04a54c72de1848766b39885942d743a
Neo <teseus@wemakeprice.com>
2020-05-29 12:37:26 +0900

    format check.

--- kotlinexercise/src/main/kotlin/com/teseus/inaction/ch6/TheLetFunction.kt
+++ kotlinexercise/src/main/kotlin/com/teseus/inaction/ch6/TheLetFunction.kt
@@ -1,6 +1,6 @@
 package com.teseus.inaction.ch6
 
-fun sendToEmail(email: String){
+fun sendToEmail(email: String) {
     println("sending Email to $email")
 }
 
@@ -9,9 +9,9 @@ fun getEmailAddres(person: Person): String? = person?.email
 fun main() {
     with(
             Person("teseus", "teseus@wemakeprice.com",
-            Company("wmp",
-                    Address("teheranro", 135030, "seoul", "Korea")))
-    ){
+                    Company("wmp",
+                            Address("teheranro", 135030, "seoul", "Korea")))
+    ) {
         //sendToEmail(getEmailAddres(this)) impossible
         getEmailAddres(this)?.let {
             sendToEmail(it)
