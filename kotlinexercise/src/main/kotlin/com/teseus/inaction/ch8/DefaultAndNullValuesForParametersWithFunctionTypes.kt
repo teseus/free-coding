4a0b1226b0dc0b64d2e5579a6c532948cd4f4ba7
Neo <teseus@wemakeprice.com>
2020-06-10 14:29:32 +0900

    function returning function

--- kotlinexercise/src/main/kotlin/com/teseus/inaction/ch8/DefaultAndNullValuesForParametersWithFunctionTypes.kt
+++ kotlinexercise/src/main/kotlin/com/teseus/inaction/ch8/DefaultAndNullValuesForParametersWithFunctionTypes.kt
@@ -3,16 +3,15 @@ package com.teseus.inaction.ch8
 fun main() {
     val seasons = listOf("Spring", "Summer", "Fall", "Winter")
     println(seasons.joinToString2())
-    println(seasons.joinToString2{it.toUpperCase()})
-
+    println(seasons.joinToString2 { it.toUpperCase() })
 }
 
-private fun <E> Collection<E>.joinToString2 (
-        pre:String = "[", sep:String = ", ", post:String = "]",
-        transform:((E) -> String)? = null):String {
+private fun <E> Collection<E>.joinToString2(
+        pre: String = "[", sep: String = ", ", post: String = "]",
+        transform: ((E) -> String)? = null): String {
     val sb = StringBuilder(pre)
-    for((idx,ele) in withIndex()){
-        if(idx>0) sb.append(sep)
+    for ((idx, ele) in withIndex()) {
+        if (idx > 0) sb.append(sep)
         val converted = transform?.invoke(ele) ?: ele.toString()
         sb.append(converted)
     }
