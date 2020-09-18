5671ba2bf414d2f9358c5b19825ee8205a7f157f
Neo <teseus@wemakeprice.com>
2020-09-18 18:48:20 +0900

    InterestingParty

---
+++ freecoding/src/main/java/com/teseus/kata/topcode/InterestingParty.java
@@ -0,0 +1,15 @@
+package com.teseus.kata.topcode;
+
+import org.junit.Assert;
+
+public class InterestingParty {
+    public static int bestInvitaion(String[] first, String[] second){
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Assert.assertEquals(2, bestInvitaion(
+                new String[]{"a","b","c"},
+                new String[]{"b","c","d"}));
+    }
+}
