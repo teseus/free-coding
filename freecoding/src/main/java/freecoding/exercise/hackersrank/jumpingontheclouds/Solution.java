3756e254ef5bce63533beb022e3f53a6bf3c3b37
Neo <teseus@wemakeprice.com>
2020-11-02 22:23:16 +0900

    [HackerRank] warm up3.

---
+++ freecoding/src/main/java/freecoding/exercise/hackersrank/jumpingontheclouds/Solution.java
@@ -0,0 +1,30 @@
+package freecoding.exercise.hackersrank.jumpingontheclouds;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution {
+
+    static int jumpingOnClouds(int[] c) {
+        int start = 0;
+        int jump = 0;
+        for (int i = 0; i < c.length; i++) {
+            if(c[i] == 1){
+               jump += ((i-start)/2) + 1;
+               start = ++i;
+            } else if (i == c.length-1) {
+                jump = (int) (jump + Math.ceil((i - start) * 1.0f / 2.0f));
+            }
+        }
+        System.out.println(jump);
+        return jump;
+    }
+
+    public static void main(String[] args) {
+        assertEquals(4, jumpingOnClouds(new int[]{0,0,1,0,0,1,0}));
+        assertEquals(3, jumpingOnClouds(new int[]{0,0,0,0,1,0}));
+        assertEquals(2, jumpingOnClouds(new int[]{0,1,0,0}));
+        assertEquals(2, jumpingOnClouds(new int[]{0,1,0,0,0}));
+        assertEquals(3, jumpingOnClouds(new int[]{0,1,0,0,0,0}));
+    }
+}
+
