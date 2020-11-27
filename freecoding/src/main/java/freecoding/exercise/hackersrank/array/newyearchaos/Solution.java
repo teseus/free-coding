3716d7c155456b0faf450c0ee58f4d08dd8ab513
Neo <teseus@wemakeprice.com>
2020-11-27 15:01:52 +0900

    coding in progress

--- freecoding/src/main/java/freecoding/exercise/hackersrank/array/newyearchaos/Solution.java
+++ freecoding/src/main/java/freecoding/exercise/hackersrank/array/newyearchaos/Solution.java
@@ -1,5 +1,7 @@
 package freecoding.exercise.hackersrank.array.newyearchaos;
 
+import java.util.Arrays;
+
 public class Solution {
 
     // Complete the minimumBribes function below.
@@ -10,7 +12,7 @@ public class Solution {
             copy[i-1] = i;
         }
 
-        for (int i = copy.length; i > 0; i--) {
+        for (int i = 1; i <= copy.length; i++) {
             int diff = q[i-1] - i;
             if(diff > 2) {
                 System.out.println("Too chaotic");
@@ -22,22 +24,26 @@ public class Solution {
             }
         }
 
-
+        System.out.println(Arrays.toString(copy));
         System.out.println(count);
     }
 
     private static void swap(int[] copy, int i, int diff) {
-        for (int j = 1; j <= diff; j++, i--) {
-            int temp = copy[i];
-            copy[i] = copy[i-j];
-            copy[i-1] = temp;
+        int temp = copy[i];
+        for (int j = i-diff; j>0; j--) {
+            copy[j+1] = copy[j];
         }
+        copy[i-diff] = temp;
     }
 
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        Solution.swap(array, 2, 2);
+        System.out.println(Arrays.toString(array));
+
 //        Solution.minimumBribes(new int[]{2,1,5,3,4});
 //        Solution.minimumBribes(new int[]{5, 1, 2, 3, 7, 8, 6, 4});
-        Solution.minimumBribes(new int[]{1, 2, 5, 3, 7, 8, 6, 4});
+//        Solution.minimumBribes(new int[]{1, 2, 5, 3, 7, 8, 6, 4});
 //        Solution.minimumBribes(new int[]{2,5,1,3,4});
     }
 }
