07b5fd95a4bf3d852dc2f6c1c5863723c216580e
Neo <teseus@wemakeprice.com>
2021-01-04 15:23:32 +0900

    [Go] grammar slice

--- goexercise/simple/firstGo.go
+++ goexercise/simple/firstGo.go
@@ -9,8 +9,25 @@ func main() {
 	println("hello go world")
 
 	printTypeValue()
+	sliceTest()
 }
 
 func printTypeValue() {
-	fmt.Printf("%T %v", 1000, 1000)
+	fmt.Printf("%T %v \n", 1000, 1000)
+}
+
+func sliceTest() {
+	x := make([]float64, 5, 10)
+	fmt.Println(x)
+
+	x = append(x, 5, 6)
+	fmt.Println(x)
+
+	arr := [5]float64{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+	x = arr[0:5]
+	fmt.Println(x)
+
+	x = arr[1:4]
+	fmt.Println(x)
 }
